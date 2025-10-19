8dbf74291df2454dc9d790f6cf7e39db6a7b7d5e
Tawose oluwatimilehin <oluwatimilehintawose2005@gmail.com>
2025-10-20 00:08:35 +0100

    check for includes folder for classes

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -2,7 +2,13 @@
 spl_autoload_register("AutoLoader");
 function AutoLoader($className)
 {
-    $path = "classes/";
+    $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    if (strpos($url, 'includes') !== false) {
+        $path = '../classes/';
+    } else {
+        $path = "classes/";
+    }
+
     $extension = ".php";
     $fullPathName = $path . $className . $extension;
     try {
@@ -10,8 +16,31 @@ function AutoLoader($className)
             throw new Exception("invalid class or file Name");
         }
         include_once $fullPathName;
-        return $fullPathName;
     } catch (ErrorException  $th) {
         echo $th->getMessage();
     }
 }
+
+
+
+
+
+
+
+
+// spl_autoload_register("AutoLoader");
+// function AutoLoader($className)
+// {
+//     $path = "classes/";
+//     $extension = ".php";
+//     $fullPathName = $path . $className . $extension;
+//     try {
+//         if (!file_exists($fullPathName)) {
+//             throw new Exception("invalid class or file Name");
+//         }
+//         include_once $fullPathName;
+//         return $fullPathName;
+//     } catch (ErrorException  $th) {
+//         echo $th->getMessage();
+//     }
+// }
